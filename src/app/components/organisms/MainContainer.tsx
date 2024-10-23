d3775e24b8794233212ff9f0f88547024ac18a4a
HugoCastroBr <hugocastrohc@outlook.com>
2024-10-23 08:24:46 -0300

    fix: container padding

--- src/app/components/organisms/MainContainer.tsx
+++ src/app/components/organisms/MainContainer.tsx
@@ -5,7 +5,7 @@ const MainContainer = styled(Container)(({ theme }) => ({
   width: '100vw',
   height: '100vh',
   paddingLeft: 80,
-  paddingRight: 0,
+  paddingRight: 20,
   [theme.breakpoints.down('sm')]: {
     paddingLeft: 2,
     paddingRight: 2,
